b4d62ed3f7807852ca3de123e642c62ba2c6febf
vaibhav52582 <79960982+vaibhav52582@users.noreply.github.com>
2021-03-03 13:08:19 +0530

    Add files via upload

---
+++ hands-on/solutions/vaibhav/oops/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace customer 
+{
+    class Mainclass
+    {
+        public static void Main(string[] args)
+        {
+            customerdetails details= new customerdetails  (9650, "vaibhav", "vaibhavdeshwal@gmail.com", 9650373595, "B sector", "GOLD");
+            details.Display();
+        }
+    }
+}
---
+++ hands-on/solutions/vaibhav/oops/customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer
+{
+    class customerdetails
+    {
+        public int Id { get; set; }
+        public string NAME { get; set; }
+        public string EMAIL { get; set; }
+        public long Mobile { get; set; }
+        public string address { get; set; }
+        public string loyaltymembership { get; set; }
+        public customerdetails(int id, string name, string email, long Mobile, string address, string loyaltymembership)
+        {
+            Id = id;
+            NAME = name;
+            EMAIL = email;
+            this.Mobile = Mobile;
+            this.address = address;
+            this.loyaltymembership = loyaltymembership;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Customer Details ");
+            Console.WriteLine(" Id        : {0}\n " +
+                              "Name       : {1}\n " +
+                              "Email      : {2}\n " +
+                              "Mobile     : {3}\n " +
+                              "Address    : {4}\n" +
+                              "Membership : {5}\n", Id, NAME, EMAIL, Mobile, address, loyaltymembership);
+        }
+    }
+}
