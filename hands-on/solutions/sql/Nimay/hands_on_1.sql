98775ed6f36c19733111ec7708d53698cc1e6989
Nimay Tiwari <50686717+nimaytiwari@users.noreply.github.com>
2021-03-11 16:35:37 +0530

    Add files via upload

---
+++ hands-on/solutions/sql/Nimay/hands_on_1.sql
@@ -0,0 +1,15 @@
+--hands on-I 
+--1)
+sp_help 'HumanResource.Employee'
+--2)
+select * from HumanResources.Employee
+--3)
+select * from HumanResources.Department
+--4)
+select LoginID, JobTitle, HireDate from HumanResources.Employee
+--5)
+EXEC sp_help 'Production.Product'
+--6)
+select ProductID, Name, StandardCost, 0.5*StandardCost from Production.Product
+--7)
+EXEC sp_help 'Person.ContactType'
