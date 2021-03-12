2d77ecbe35c324e3135ee14176db53db6d3947e5
Nimay Tiwari <50686717+nimaytiwari@users.noreply.github.com>
2021-03-12 20:15:16 +0530

    Add files via upload

---
+++ hands-on/solutions/sql/Nimay/Hands_on_III.sql
@@ -0,0 +1,18 @@
+--Hands-on-III
+--Query-I
+select * from Person.Person where MiddleName is Not null
+--Query-II
+select UPPER(CONCAT(FirstName,' ',MiddleName,' ',LastName)) As Name from Person.Person
+--Query-III
+select SalesOrderDetailID,ROUND(LineTotal,2)AsLINETOTAL from Sales.SalesOrderDetail ORDER BY SalesOrderDetailID
+--Query-IV
+SELECT CURRENT_TIMESTAMP;
+--Query-V
+select * from HumanResources.Employee where DATEDIFF(year, HireDate, CURRENT_TIMESTAMP)>10 ORDER BY HireDate
+--Query-VI
+select * from Sales.SalesOrderHeader where FORMAT(OrderDate,'MM')=02
+--Query-VII
+select ProductID,Name, COALESCE(Color,'No color') AS Color from Production.Product 
+--Query-VIII
+select * ,FORMAT(BirthDate,'MMMM dd,yyyy')As BirthDate --CONVERT(datetime,BirthDate,107)As BirthDate 
+from HumanResources.Employee
