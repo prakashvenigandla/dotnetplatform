23dedee74e586f09589dc263ea03d7914eb07e6a
Rinki0712 <79964244+Rinki0712@users.noreply.github.com>
2021-03-14 13:35:18 +0530

    Add files via upload

---
+++ 2. ElectricityBill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace handsOn
+{
+    class Electricity
+    {
+       public static void Main(string[] args)
+        {
+            int unit=0, amount=0;
+            Console.WriteLine("\n------------ Welcome to Bill Counter ---------------\n");
+            Console.WriteLine("Enter number of units to calculate bill amount");
+            unit=Convert.ToInt32(Console.ReadLine());
+            if(unit>=0&&unit<=100)
+            {
+                amount = unit * 3;
+            }
+            else if(unit>=101&&unit<=200)
+            {
+                amount = unit * 4;
+            }
+            else if(unit>=201&&unit<=400)
+            {
+                amount = unit * 5;
+            }
+            Console.WriteLine("The amount of electricity bill is :- "+amount);
+
+        }
+    }
+
+   
+}
