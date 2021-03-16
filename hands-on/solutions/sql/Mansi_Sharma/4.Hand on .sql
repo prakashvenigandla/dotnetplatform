f20510ad3145b9ef23434ebac08c4486a18652b0
mansi-sharma937 <79961336+mansi-sharma937@users.noreply.github.com>
2021-03-16 10:12:28 +0530

    Update 4.Hand on .sql

--- hands-on/solutions/sql/Mansi_Sharma/4.Hand on .sql
+++ hands-on/solutions/sql/Mansi_Sharma/4.Hand on .sql
@@ -10,25 +10,27 @@ select SalesOrderID ,MAX(LineTotal) as Highest_LineTotal,MIN(LineTotal) as Lowes
 from Sales.SalesOrderDetail where SalesOrderID ='43659' group by SalesOrderID
 
 --5)
-select * from Sales.SalesOrderDetail
 select SalesOrderID  ,MAX(LineTotal) as Highest_LineTotal,MIN(LineTotal) as Lowest_LineTotal,avg(LineTotal) as Avg_LineTotal 
 from Sales.SalesOrderDetail group by SalesOrderID
 
 --6)
-select * from HumanResources.Employee
-select * from HumanResources.EmployeeDepartmentHistory
-select * from Sales.SalesOrderDetail
-select * from Sales.SalesOrderHeader
-
 select JobTitle ,count(*) from HumanResources.Employee group by JobTitle
 
---7) doubt
+--7)
+select count(DepartmentID) as count, BusinessEntityID from HumanResources.EmployeeDepartmentHistory 
+where EndDate is not null group by BusinessEntityId
 
 --8)
 select SalesOrderID ,SubTotal as Order_value from Sales.SalesOrderHeader where SubTotal>'25000'
 
---9) doubt
+--9) 
+select DepartmentID,count(BusinessEntityID)As Total_Employee from HumanResources.EmployeeDepartmentHistory group by DepartmentID
 
---10)doubt
+
+--10)
+select count(DepartmentID) as [Dept_Experience] ,BusinessEntityID from HumanResources.EmployeeDepartmentHistory 
+group by BusinessEntityID having count(DepartmentID)>2
+
+
 
 
